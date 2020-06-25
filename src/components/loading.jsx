5b684fb2c63cceaeb4dea1efd074990c471c1115
Rhian Castro <rhi.castro@gmail.com>
2020-06-25 16:30:24 -0300

    Refatorando para o codeclimate

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,31 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
 
-function App() {
+function Loading() {
   return (
-    <div id="meals">
-      <Router>
-        <Switch>
-          <Route to="/">{}</Route>
-          <Route to="/comidas">{}</Route>
-          <Route to="/bebidas">{}</Route>
-          <Route to="/comidas/:id">{}</Route>
-          <Route to="/bebidas/:id">{}</Route>
-          <Route to="/comidas/:id/in-progress">{}</Route>
-          <Route to="/bebidas/:id/in-progress">{}</Route>
-          <Route to="/explorar">{}</Route>
-          <Route to="/explorar/comidas">{}</Route>
-          <Route to="/explorar/bebidas">{}</Route>
-          <Route to="/explorar/comidas/ingredientes">{}</Route>
-          <Route to="/explorar/bebidas/ingredientes">{}</Route>
-          <Route to="/explorar/comidas/area">{}</Route>
-          <Route to="/perfil">{}</Route>
-          <Route to="/receitas-feitas">{}</Route>
-          <Route to="/receitas-favoritas">{}</Route>
-          <Route to="*">{}</Route>
-        </Switch>
-      </Router>
+    <div>
       <span>TRYBE</span>
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -89,4 +66,4 @@ function App() {
   );
 }
 
-export default App;
+export default Loading;
