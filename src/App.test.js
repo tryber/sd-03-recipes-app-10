de35089ddc3feeffee9ecc019b55f0c0a978609e
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-06-26 16:53:59 -0300

    Minor changes made

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,6 @@ import App from './App';
 
 test('Farewell, front-end', () => {
   renderWithRouter(<App />);
-  const appReceitas = document.querySelector('#app-receitas')
+  const appReceitas = document.querySelector('#app-receitas');
   expect(appReceitas).toBeInTheDocument();
 });
-
