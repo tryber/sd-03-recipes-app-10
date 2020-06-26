63624434a2080d248526ef1eec5113a22c73ff70
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-06-26 10:39:29 -0300

    Codeclimate Check 2

--- src/Edu/Header.jsx
+++ src/Edu/Header.jsx
@@ -8,46 +8,43 @@ export default function Header() {
   const [toggleSearchBar, setToggleSearchBar] = useState(false);
 
   useEffect(() => {
-    setTitle("Comidas");
+    setTitle('Comidas');
     setIsSearchable(true);
-  },[])
+  }, []);
 
-  const renderSearchBar = () => {
+  const renderSearchBar = () =>
     toggleSearchBar === true
       ? setToggleSearchBar(false)
       : setToggleSearchBar(true);
-  };
 
   return (
     <div>
-      <header className='header'>
-        <Link to='/perfil'>
+      <header className="header">
+        <Link to="/perfil">
           <button
-            data-testid='profile-top-btn'
-            type='button'
-            className='profile-icon'
+            data-testid="profile-top-btn"
+            type="button"
+            className="profile-icon"
           />
         </Link>
-        <div className='title'>
-          <p data-testid='page-title'>{title}</p>
+        <div className="title">
+          <p data-testid="page-title">{title}</p>
         </div>
         {isSearchable ? (
           <button
-            data-testid='search-top-btn'
+            data-testid="search-top-btn"
             onClick={renderSearchBar}
-            type='button'
-            className='search-icon'
+            type="button"
+            className="search-icon"
           />
         ) : null}
       </header>
       {toggleSearchBar ? (
-
-          <input
-            className='search-bar'
-            data-testid='search-input'
-            placeholder='Buscar Receitas'
-          />
-
+        <input
+          className="search-bar"
+          data-testid="search-input"
+          placeholder="Buscar Receitas"
+        />
       ) : null}
       {/* {toggleSearchBar ? (
         <SearchBar />
