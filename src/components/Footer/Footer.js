9116ff135db7755a6372bb4e3eff1cb82639b9aa
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-06-29 19:00:17 -0300

    Adding the routes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,28 +5,24 @@ import exploreIcon from '../../images/exploreIcon.svg';
 import drinkIcon from '../../images/drinkIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
 
-const imageDisplay = (dataTestid, imageClassNameAndAlt, imageSrc) => {
-  return(
-    <img
-      data-testid={dataTestid}
-      alt={imageClassNameAndAlt}
-      className={imageClassNameAndAlt}
-      src={imageSrc}
-    />
-  )
-}
+const imageDisplay = (dataTestid, imageClassNameAndAlt, imageSrc) => <img
+    data-testid={dataTestid}
+    alt={imageClassNameAndAlt}
+    className={imageClassNameAndAlt}
+    src={imageSrc}
+  />
 
 export default function Footer() {
   return (
     <div className="footer-container" data-testid="footer">
       <Link to="/bebidas">
-        {imageDisplay("drinks-bottom-btn", "drink-icon", drinkIcon)}
+        {imageDisplay('drinks-bottom-btn', 'drink-icon', drinkIcon)}
       </Link>
       <Link to="/explorar">
-        {imageDisplay("explore-bottom-btn", "explore-icon", exploreIcon)}
+        {imageDisplay('explore-bottom-btn', 'explore-icon', exploreIcon)}
       </Link>
       <Link to="/comidas">
-        {imageDisplay("food-bottom-btn", "meal-icon", mealIcon)}
+        {imageDisplay('food-bottom-btn', 'meal-icon', mealIcon)}
       </Link>
     </div>
   );
