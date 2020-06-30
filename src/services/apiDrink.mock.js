9b55be1d3ecd2242ccf344a25c3286b0fab203d4
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-06-30 15:30:12 -0300

    CodeClimate check 27

--- src/services/apiDrink.mock.js
+++ src/services/apiDrink.mock.js
@@ -1,139 +1,133 @@
 export const mockGetDrinkList = {
   drinks: [
     {
-      idDrink: "15997",
-      strDrink: "GG",
-      strCategory: "Ordinary Drink",
-      strAlcoholic: "Optional alcohol",
-      strGlass: "Collins Glass",
+      idDrink: '15997',
+      strDrink: 'GG',
+      strCategory: 'Ordinary Drink',
+      strAlcoholic: 'Optional alcohol',
+      strGlass: 'Collins Glass',
       strInstructions:
-        "Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG.",
+        'Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG.',
       strInstructionsDE:
-        "Den Galliano-Likör über Eis gießen. Füllen Sie den Rest des Glases mit Ginger Ale und das ist alles, was dazu gehört. Du hast jetzt ein eigenes GG.",
-      strDrinkThumb:
-        "https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg",
-      strIngredient1: "Galliano",
-      strIngredient2: "Ginger ale",
-      strIngredient3: "Ice",
-      strMeasure1: "2 1/2 shots ",
-      strCreativeCommonsConfirmed: "No",
-      dateModified: "2016-07-18 22:06:00",
+        'Den Galliano-Likör über Eis gießen. Füllen Sie den Rest des Glases mit Ginger Ale und das ist alles, was dazu gehört. Du hast jetzt ein eigenes GG.',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+      strIngredient1: 'Galliano',
+      strIngredient2: 'Ginger ale',
+      strIngredient3: 'Ice',
+      strMeasure1: '2 1/2 shots ',
+      strCreativeCommonsConfirmed: 'No',
+      dateModified: '2016-07-18 22:06:00',
     },
   ],
 };
 
 export const mockGetDrinksByCategorie = {
-  foundBy: "Cocoa",
+  foundBy: 'Cocoa',
   drinks: [
     {
-      strDrink: "Castillian Hot Chocolate",
-      strDrinkThumb:
-        "https://www.thecocktaildb.com/images/media/drink/3nbu4a1487603196.jpg",
-      idDrink: "12730",
+      strDrink: 'Castillian Hot Chocolate',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/3nbu4a1487603196.jpg',
+      idDrink: '12730',
     },
   ],
 };
 
 export const mockGetDrinksCategoriesList = {
-  drinks: [{ strCategory: "Ordinary Drink" }],
+  drinks: [{ strCategory: 'Ordinary Drink' }],
 };
 
 export const mockGetDrinkByIngredient = {
-  foundBy: "Gin",
+  foundBy: 'Gin',
   drinks: [
     {
-      strDrink: "69 Special",
-      strDrinkThumb:
-        "https://www.thecocktaildb.com/images/media/drink/vqyxqx1472669095.jpg",
-      idDrink: "13940",
+      strDrink: '69 Special',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vqyxqx1472669095.jpg',
+      idDrink: '13940',
     },
   ],
 };
 
 export const mockGetDrinkByName = {
-  foundBy: "Gin",
+  foundBy: 'Gin',
   drinks: [
     {
-      idDrink: "11410",
-      strDrink: "Gin Fizz",
-      strTags: "IBA,Classic",
-      strVideo: "https://www.youtube.com/watch?v=obGhGNUKx30",
-      strCategory: "Ordinary Drink",
-      strIBA: "Unforgettables",
-      strAlcoholic: "Alcoholic",
-      strGlass: "Highball glass",
+      idDrink: '11410',
+      strDrink: 'Gin Fizz',
+      strTags: 'IBA,Classic',
+      strVideo: 'https://www.youtube.com/watch?v=obGhGNUKx30',
+      strCategory: 'Ordinary Drink',
+      strIBA: 'Unforgettables',
+      strAlcoholic: 'Alcoholic',
+      strGlass: 'Highball glass',
       strInstructions:
-        "Shake all ingredients with ice cubes, except soda water. Pour into glass. Top with soda water.",
+        'Shake all ingredients with ice cubes, except soda water. Pour into glass. Top with soda water.',
       strInstructionsDE:
-        "Alle Zutaten mit Eiswürfel schütteln, außer Sodawasser. In das Glas gießen. Mit Sodawasser auffüllen.",
-      strDrinkThumb:
-        "https://www.thecocktaildb.com/images/media/drink/xhl8q31504351772.jpg",
-      strIngredient1: "Gin",
-      strIngredient2: "Lemon",
-      strIngredient3: "Powdered sugar",
-      strIngredient4: "Carbonated water",
-      strMeasure1: "2 oz ",
-      strMeasure2: "Juice of 1/2 ",
-      strMeasure3: "1 tsp ",
-      strCreativeCommonsConfirmed: "No",
-      dateModified: "2017-09-02 12:29:32",
+        'Alle Zutaten mit Eiswürfel schütteln, außer Sodawasser. In das Glas gießen. Mit Sodawasser auffüllen.',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/xhl8q31504351772.jpg',
+      strIngredient1: 'Gin',
+      strIngredient2: 'Lemon',
+      strIngredient3: 'Powdered sugar',
+      strIngredient4: 'Carbonated water',
+      strMeasure1: '2 oz ',
+      strMeasure2: 'Juice of 1/2 ',
+      strMeasure3: '1 tsp ',
+      strCreativeCommonsConfirmed: 'No',
+      dateModified: '2017-09-02 12:29:32',
     },
   ],
 };
 
 export const mockGetDrinkByFirstLetter = {
-  foundBy: "e",
+  foundBy: 'e',
   drinks: [
     {
-      idDrink: "12668",
-      strDrink: "Egg Cream",
-      strCategory: "Other/Unknown",
-      strAlcoholic: "Non alcoholic",
-      strGlass: "Coffee mug",
+      idDrink: '12668',
+      strDrink: 'Egg Cream',
+      strCategory: 'Other/Unknown',
+      strAlcoholic: 'Non alcoholic',
+      strGlass: 'Coffee mug',
       strInstructions:
-        "Mix syrup and milk in a fountain glass. Add soda water, serve with a straw.",
+        'Mix syrup and milk in a fountain glass. Add soda water, serve with a straw.',
       strInstructionsDE:
-        "Sirup und Milch in einem Fontänenglas mischen. Mit Sodawasser aufgießen und mit einem Strohhalm servieren.",
-      strDrinkThumb:
-        "https://www.thecocktaildb.com/images/media/drink/mvis731484430445.jpg",
-      strIngredient1: "Chocolate syrup",
-      strIngredient2: "Milk",
-      strIngredient3: "Soda water",
-      strMeasure1: "2 tblsp ",
-      strMeasure2: "6 oz whole ",
-      strMeasure3: "6 oz ",
-      strCreativeCommonsConfirmed: "No",
-      dateModified: "2017-01-14 21:47:26",
+        'Sirup und Milch in einem Fontänenglas mischen. Mit Sodawasser aufgießen und mit einem Strohhalm servieren.',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/mvis731484430445.jpg',
+      strIngredient1: 'Chocolate syrup',
+      strIngredient2: 'Milk',
+      strIngredient3: 'Soda water',
+      strMeasure1: '2 tblsp ',
+      strMeasure2: '6 oz whole ',
+      strMeasure3: '6 oz ',
+      strCreativeCommonsConfirmed: 'No',
+      dateModified: '2017-01-14 21:47:26',
     },
   ],
 };
 
 export const mockGetDrinkByID = {
-  foundBy: "14356",
+  foundBy: '14356',
   drinks: [
     {
-      idDrink: "14356",
-      strDrink: "Blind Russian",
-      strCategory: "Milk / Float / Shake",
-      strAlcoholic: "Alcoholic",
-      strGlass: "Collins glass",
-      strInstructions: "Fill glass with ice. Add all liquers. Add milk. shake.",
+      idDrink: '14356',
+      strDrink: 'Blind Russian',
+      strCategory: 'Milk / Float / Shake',
+      strAlcoholic: 'Alcoholic',
+      strGlass: 'Collins glass',
+      strInstructions: 'Fill glass with ice. Add all liquers. Add milk. shake.',
       strInstructionsDE:
-        "Füllen Sie das Glas mit Eis. Füge alle Liköre hinzu. Milch zugeben. Schütteln.",
-      strDrinkThumb:
-        "https://www.thecocktaildb.com/images/media/drink/wxuqvr1472720408.jpg",
-      strIngredient1: "Baileys irish cream",
-      strIngredient2: "Godiva liqueur",
-      strIngredient3: "Kahlua",
-      strIngredient4: "Butterscotch schnapps",
-      strIngredient5: "Milk",
-      strMeasure1: "3/4 oz ",
-      strMeasure2: "3/4 oz ",
-      strMeasure3: "3/4 oz ",
-      strMeasure4: "1/2 oz ",
-      strMeasure5: " to fill\n",
-      strCreativeCommonsConfirmed: "No",
-      dateModified: "2016-09-01 10:00:08",
+        'Füllen Sie das Glas mit Eis. Füge alle Liköre hinzu. Milch zugeben. Schütteln.',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/wxuqvr1472720408.jpg',
+      strIngredient1: 'Baileys irish cream',
+      strIngredient2: 'Godiva liqueur',
+      strIngredient3: 'Kahlua',
+      strIngredient4: 'Butterscotch schnapps',
+      strIngredient5: 'Milk',
+      strMeasure1: '3/4 oz ',
+      strMeasure2: '3/4 oz ',
+      strMeasure3: '3/4 oz ',
+      strMeasure4: '1/2 oz ',
+      strMeasure5: ' to fill\n',
+      strCreativeCommonsConfirmed: 'No',
+      dateModified: '2016-09-01 10:00:08',
     },
   ],
 };
