d8793d86a7a4a398db2cfc39f48df0860ac90189
Rhian Castro <rhi.castro@gmail.com>
2020-06-30 15:40:29 -0300

    profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
@@ -6,6 +7,19 @@ export default function Profile() {
   return (
     <div>
       {Header('Perfil', false)}
+      <h1>ProfilePage</h1>
+      <h2>{user}</h2>
+      <Link to="/receitas-favoritas">
+        <button data-testid="profile-done-btn">Receitas Favoritas</button>
+      </Link>
+      <Link to="/receitas-feitas">
+        <button data-testid="profile-favorite-btn">Receitas Feitas</button>
+      </Link>
+      <Link to="/">
+        <button onClick={() => localStorage.clear()} data-testid="profile-logout-btn">
+          Sair
+        </button>
+      </Link>
       <Footer />
     </div>
   );
