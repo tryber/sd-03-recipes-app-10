99e71064bec2effc986c82f72282cace3c59cb4f
Rhian Castro <rhi.castro@gmail.com>
2020-06-30 16:57:07 -0300

    fix margin, position and maybe font?

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,25 +3,27 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
-const user = localStorage.getItem('user');
+const { email } = JSON.parse(localStorage.getItem('user'));
 
 export default function Profile() {
   return (
     <div>
       {Header('Perfil', false)}
-      <h1>ProfilePage</h1>
-      <h2>{user}</h2>
-      <Link to="/receitas-favoritas">
-        <button data-testid="profile-favorite-btn">Receitas Favoritas</button>
-      </Link>
-      <Link to="/receitas-feitas">
-        <button data-testid="profile-done-btn">Receitas Feitas</button>
-      </Link>
-      <Link to="/">
-        <button onClick={() => localStorage.clear()} data-testid="profile-logout-btn">
-          Sair
-        </button>
-      </Link>
+      <div className="holder">
+        <h2>{email}</h2>
+        <Link to="/receitas-favoritas">
+          <button data-testid="profile-favorite-btn">Receitas Favoritas</button>
+        </Link>
+        <Link to="/receitas-feitas">
+          <button data-testid="profile-done-btn">Receitas Feitas</button>
+        </Link>
+        <Link to="/">
+          <button onClick={() => localStorage.clear()} data-testid="profile-logout-btn">
+            Sair
+          </button>
+          {console.log(email)}
+        </Link>
+      </div>
       <Footer />
     </div>
   );
