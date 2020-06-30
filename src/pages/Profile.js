2d469d00040263cfca7a335080a7e522b5dbf7d6
Rhian Castro <rhi.castro@gmail.com>
2020-06-30 15:47:11 -0300

    cc fix

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+
 const user = localStorage.getItem('user');
 
 export default function Profile() {
