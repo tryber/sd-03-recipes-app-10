2a35822a7ea9886cca68b387aa2768ea02c5c8a4
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-06-30 22:58:23 -0300

    Codeclimate check 33

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,67 +1,64 @@
-import React ,{useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import './SearchBar.style.css';
-import * as Api from '../../services/api'
+import * as Api from '../../services/api';
 import RecipeContext from '../../Context/RecipeContext';
 
 const filterFoodLogic = async (category, text, setFoodValues) => {
-  if(category === 'Ingrediente' && text){
-    return setFoodValues(await Api.getFoodByIngredient(text))
-  } 
+  if (category === 'Ingrediente' && text) {
+    return setFoodValues(await Api.getFoodByIngredient(text));
+  }
   if (category === 'Nome' && text) {
-    return setFoodValues(await Api.getFoodByName(text))
-  } 
+    return setFoodValues(await Api.getFoodByName(text));
+  }
   if (category === 'Primeira letra' && text.length === 1)
-    return setFoodValues(await Api.getFoodByFirstLetter(text))
-}
+    return setFoodValues(await Api.getFoodByFirstLetter(text));
+};
 
 const filterDrinkLogic = async (category, text, setDrinkValues) => {
-  if(category === 'Ingrediente' && text){
-    return setDrinkValues(await Api.getDrinkByIngredient(text))
-  } 
+  if (category === 'Ingrediente' && text) {
+    return setDrinkValues(await Api.getDrinkByIngredient(text));
+  }
   if (category === 'Nome' && text) {
-    return setDrinkValues(await Api.getDrinkByName(text))
-  } 
+    return setDrinkValues(await Api.getDrinkByName(text));
+  }
   if (category === 'Primeira letra' && text.length === 1)
-    return setDrinkValues(await Api.getDrinkByFirstLetter(text))
-}
+    return setDrinkValues(await Api.getDrinkByFirstLetter(text));
+};
 
 const filteredSearch = async (e, currentPath, category, text, setFoodValues, setDrinkValues) => {
   e.preventDefault();
   console.log(text, category, currentPath);
-  if(currentPath === '/comidas') {
-    filterFoodLogic(category, text, setFoodValues);
+  if (currentPath === '/comidas') {
+    return filterFoodLogic(category, text, setFoodValues);
   }
-  if(currentPath === '/bebidas') {
-    filterDrinkLogic(category, text, setDrinkValues)
-}
-}
+  if (currentPath === '/bebidas') {
+    return filterDrinkLogic(category, text, setDrinkValues);
+  }
+};
+
+const radioBtnDisplay = (className, type, name, id, value, testid, func) => (
+  <div>
+    <input
+      className={className}
+      type={type}
+      name={name}
+      id={id}
+      value={value}
+      data-testid={testid}
+      onClick={func}
+    />
+    <label htmlFor={id}>{value}</label>
+  </div>
+);
 
 export default function SearchBar() {
-  const { setFoodValues, setDrinkValues } = useContext(
-  RecipeContext);
+  const { setFoodValues, setDrinkValues } = useContext(RecipeContext);
 
   const [text, setText] = useState('');
-  const [category, setCategory] = useState(null)
+  const [category, setCategory] = useState(null);
   const currentPath = window.location.pathname;
-  const radioBtnDisplay = (className, type, name, id, value, testid, func) => (
-    <div>
-      <input
-        className={className}
-        type={type}
-        name={name}
-        id={id}
-        value={value}
-        data-testid={testid}
-        onClick={func}
-      />
-      <label htmlFor={id}>{value}</label>
-    </div>
-  );
 
-  const saveValues = (e) => {
-    console.log(e.target.value);
-    return setCategory(e.target.value);
-  };
+  const saveValues = (e) => setCategory(e.target.value);
 
   return (
     <div>
@@ -74,16 +71,23 @@ export default function SearchBar() {
         />
       </div>
       <div className="search-btn-container">
-        <button onClick={(e) => filteredSearch(e, currentPath, category, text, setFoodValues, setDrinkValues)} data-testid="exec-search-btn" className="search-btn-display" type="submit">
+        <button
+          onClick={(e) =>
+            filteredSearch(e, currentPath, category, text, setFoodValues, setDrinkValues)
+          }
+          data-testid="exec-search-btn"
+          className="search-btn-display"
+          type="submit"
+        >
           Buscar
         </button>
       </div>
       <form className="radio-btn-container">
-        {radioBtnDisplay('radio-btn', 'radio', 'select', 'Ingrediente', 'Ingrediente'
+        {radioBtnDisplay( 'radio-btn', 'radio', 'select', 'Ingrediente', 'Ingrediente'
         , 'ingredient-search-radio', saveValues)}
-        {radioBtnDisplay('radio-btn', 'radio', 'select', 'Nome', 'Nome'
+        {radioBtnDisplay( 'radio-btn', 'radio', 'select', 'Nome', 'Nome'
         , 'name-search-radio', saveValues)}
-        {radioBtnDisplay('radio-btn', 'radio', 'select', 'Primeira letra', 'Primeira letra'
+        {radioBtnDisplay( 'radio-btn', 'radio', 'select', 'Primeira letra', 'Primeira letra'
         , 'first-letter-search-radio', saveValues)}
       </form>
     </div>
