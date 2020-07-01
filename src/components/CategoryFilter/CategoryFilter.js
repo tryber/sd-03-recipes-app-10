020f066ea9b935ffda298f7b2efde223c2591249
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-07-01 11:09:43 -0300

    upadno

--- src/components/CategoryFilter/CategoryFilter.js
+++ src/components/CategoryFilter/CategoryFilter.js
@@ -6,19 +6,23 @@ export default function CategoryFilter(apiToCallFilters, valueToMap) {
   const { valueToFilter, setValueToFilter } = useContext(RecipeContext);
   const [objectReturnedAfterReq, setObjectReturnedAfterReq] = useState(null);
 
-  const categoryButton = (el, index) => index <= 4 &&
-    <button
-      data-testid={`${el.strCategory}-category-filter`}
-      onClick={() => changeFilterValue(el.strCategory)}
-    >{el.strCategory}</button>;
-
-  const functionToMakeRequisition = async () => {
-    setObjectReturnedAfterReq(await apiToCallFilters());
-  };
-
   const changeFilterValue = (val) => {
     if (val === valueToFilter) return setValueToFilter('All');
     return setValueToFilter(val);
+  };
+  const categoryButton = (el, index) => index <= 4
+    && (
+    <button
+      data-testid={`${el.strCategory}-category-filter`}
+      onClick={() => changeFilterValue(el.strCategory)}
+      type="button"
+    >
+      {el.strCategory}
+    </button>
+    );
+
+  const functionToMakeRequisition = async () => {
+    setObjectReturnedAfterReq(await apiToCallFilters());
   };
 
   useEffect(() => {
--- src/services/api.js
+++ src/services/api.js
@@ -33,7 +33,6 @@ export const getDrinkByFirstLetter = async (letter) => {
 };
 
 export const getDrinkByID = async (id) => {
-  console.log(id)
   const drinksByID = `${initialDrinksURL}lookup.php?i=${id}`;
   return getData(drinksByID);
 };
