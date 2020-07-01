b48a73398e3e7dcea54159a60f2b1156b7b133d0
nato-x <alvares.renato@hotmail.com>
2020-07-01 17:21:59 +0000

    Prevent unlikly behavior for searchBar, searching for unvalid igredient

--- src/components/FoodsAndDrinksDisplay/FoodsAndDrinksDisplay.js
+++ src/components/FoodsAndDrinksDisplay/FoodsAndDrinksDisplay.js
@@ -20,34 +20,45 @@ const FoodsAndDrinksDisplay = (getitemDefined, stringObject, imgDisplay) => {
     return setValueToFilter('All');
   }, []);
 
-  return objectReturnedAfterReq === null ? (
-    <h1>Carregando...</h1>
-  ) : (
-    <div className="item-overflow">
-      {objectReturnedAfterReq[Object.keys(objectReturnedAfterReq)[0]].filter(filterRecipes).map(
-        (el, index) =>
-          index < 12 && (
-            <Link
-              className="container-display"
-              key={el[stringObject]}
-              data-testid={`${index}-recipe-card`}
-              to={
-                (Object.keys(objectReturnedAfterReq)[0] === 'meals' && `/comidas/${el.idMeal}`) ||
-                (Object.keys(objectReturnedAfterReq)[0] === 'drinks' && `/bebidas/${el.idDrink}`)
-              }
-            >
-              <h3 data-testid={`${index}-card-name`}>{el[stringObject]}</h3>
-              <img
-                className="img-display"
-                data-testid={`${index}-card-img`}
-                src={el[imgDisplay]}
-                alt={`${el[stringObject]}`}
-              />
-            </Link>
-          ),
-      )}
-    </div>
-  );
+  const renderDisplay = () => {
+    const firstKey = objectReturnedAfterReq !== null && Object.keys(objectReturnedAfterReq)[0];
+    console.log(firstKey);
+    console.log(objectReturnedAfterReq);
+    switch (true) {
+      case objectReturnedAfterReq === null:
+        return <h1>Carregando...</h1>;
+      case objectReturnedAfterReq[firstKey] === null:
+        return alert("Sinto muito, não encontramos nenhuma receita para esses filtros.");
+      default:
+        return (
+          <div className="item-overflow">
+            {objectReturnedAfterReq[firstKey].filter(filterRecipes)
+            .map(
+              (el, index) => index < 12 && (
+              <Link
+                className="container-display"
+                key={el[stringObject]}
+                data-testid={`${index}-recipe-card`}
+                to={
+                  (firstKey === 'meals' && `/comidas/${el.idMeal}`)
+                  || (firstKey === 'drinks' && `/bebidas/${el.idDrink}`)
+                }
+              >
+                <h3 data-testid={`${index}-card-name`}>{el[stringObject]}</h3>
+                <img
+                  className="img-display"
+                  data-testid={`${index}-card-img`}
+                  src={el[imgDisplay]}
+                  alt={`${el[stringObject]}`}
+                />
+              </Link>
+              ),
+            )}
+          </div>
+        );
+    }
+  };
+  return renderDisplay()
 };
 
 export default FoodsAndDrinksDisplay;
