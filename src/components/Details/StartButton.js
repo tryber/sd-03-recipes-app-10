974a38cba5dcd283b1836333c3fa2509f363d379
nato-x <alvares.renato@hotmail.com>
2020-07-05 18:45:59 +0000

    more CC fixes

--- src/components/Details/StartButton.js
+++ src/components/Details/StartButton.js
@@ -19,26 +19,28 @@ const drinksOrMeals = typeRequsition === 'comidas' ? 'meals' : 'drinks';
 const inProgressKey = typeRequsition === 'comidas' ? 'meals' : 'cocktails';
 
 const isRecipeInProgress = !!inProgressRecipes[inProgressKey]
-  && inProgressRecipes[inProgressKey].hasOwnProperty(itemId);
+  && Object.prototype.hasOwnProperty.call(inProgressRecipes[inProgressKey], itemId);
 
 const setRecipeToInProgress = (recipe) => {
   const recipeObj = recipe[drinksOrMeals][0];
-  const ingredients = (counter = 0) => Object.entries(recipeObj).reduce(
-    (acc = [], [key, value]) => {
-      if (key.includes('strIngredient') && !!value) {
-        acc.push(value);
-      }
-      if (key.includes('strMeasure') && !!acc[counter]) {
-        acc[counter] = `${acc[counter]} - ${value}`;
-        counter += 1;
-      }
-      console.log(acc);
-      return acc;
-    }, [],
-  );
-  return typeRequsition === 'comidas'
-    ? saveInProgressRecipes('meals', itemId, ingredients(recipe))
-    : saveInProgressRecipes('cocktails', itemId, ingredients(recipe));
+  const ingredients = () => {
+    let counter = 0;
+    Object.entries(recipeObj).reduce(
+      (acc = [], [key, value]) => {
+        if (key.includes('strIngredient') && !!value) {
+          acc.push(value);
+        }
+        if (key.includes('strMeasure') && !!acc[counter]) {
+          acc[counter] = `${acc[counter]} - ${value}`;
+          counter += 1;
+        }
+        console.log(acc);
+        return acc;
+      }, [],
+    );
+  };
+  return saveInProgressRecipes(drinksOrMeals, itemId, ingredients(recipe));
+  
 };
 
 export default function StartButton({ recipe }) {
