eaea7de06df3bfc34105b906b46d46186b6e4ded
nato-x <alvares.renato@hotmail.com>
2020-07-06 15:41:48 +0000

    git add .recomendetions[key] before recomendations map

--- src/components/FoodsAndDrinksDisplay/FoodsAndDrinksDisplay.test.js
+++ src/components/FoodsAndDrinksDisplay/FoodsAndDrinksDisplay.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { waitForElement, fireEvent } from '@testing-library/react';
 import FoodsAndDrinksDisplay from './FoodsAndDrinksDisplay';
 import renderWithRouter from '../helpers/renderWithRouter';
-import { waitForElement } from '@testing-library/react';
 
 it('should renders "Carregando..."', () => {
   const { getByText } = renderWithRouter(<FoodsAndDrinksDisplay />);
@@ -13,14 +13,19 @@ describe('Test to check if all elements are being rendered correctly on screen.'
   test('should all elements have their correct data-testids', async () => {
     const { getByTestId } = renderWithRouter(<FoodsAndDrinksDisplay />);
     const [receitaUm, receitadDoze] = await waitForElement(() => [
-      getByTestId("0-recipe-card"),
-      getByTestId("11-recipe-card")
+      getByTestId('0-recipe-card'),
+      getByTestId('11-recipe-card'),
     ]);
 
     await expect(receitaUm).toBeInTheDocument();
-    await expect(receitaUm).toHaveTextContent("Corba");
+    await expect(receitaUm).toHaveTextContent('Corba');
 
     await expect(receitadDoze).toBeInTheDocument();
-    await expect(receitadDoze).toHaveTextContent("Pancakes");
+    await expect(receitadDoze).toHaveTextContent('Pancakes');
+    const detailsBtn = getByTestId('0-card-img');
+
+    fireEvent.click(detailsBtn);
+
+    expect(window.location.pathname.includes('/comidas/52977')).toBe(true);
   });
 });
